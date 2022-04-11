c1d10383b826c89b03e76c66b0459f043eccdbb0
andruxkiller7 <cristianandruortegavargas@hotmail.com>
2022-04-11 00:25:52 -0400

    P7

---
+++ P7WebServiceUnity/Assets/Scripts/Ranks.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class Ranks 
+{
+    public string idPlayer;
+    public string puntosXP;
+    public string orbs;
+    public string damage;
+    public string stylishPTS;
+    public string itemUse;
+    public string time;
+    public string devilHunterRank;
+    public string rankBonus;
+    
+    
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ P7WebServiceUnity/Assets/Scripts/Skins.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Skins
+{
+    public int Id;
+    public string Name;
+    public string Code;
+
+
+}
+   
+
