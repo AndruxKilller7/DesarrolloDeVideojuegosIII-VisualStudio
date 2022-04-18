4591f7f1b7103d5ad49f14b2e020d2698dc804fe
andruxkiller7 <cristianandruortegavargas@hotmail.com>
2022-04-18 01:47:56 -0400

    P7-Corregido

--- P7WebServiceUnity/Assets/Scripts/SkinRequest.cs
+++ P7WebServiceUnity/Assets/Scripts/SkinRequest.cs
@@ -8,12 +8,17 @@ public class SkinRequest : MonoBehaviour
 {
 
     public Text playerName;
-    public Text skinName;
-    public Text skinCode;
+    public Text[] skinsName;
+    public Text[] skinsCode;
     public int indiceSkin;
     public Sprite[] sprites;
-    public Image contenedorDeSkins;
-   
+    public Image[] contenedorDeSkins;
+    public GameObject skinConteiner;
+    public GameObject[] skinView;
+    public Transform pivotTextSkin;
+    public GameObject padreTextCanvas;
+    public skins dates;
+    
     void Start()
     {
         
@@ -25,7 +30,7 @@ public class SkinRequest : MonoBehaviour
     void Update()
     {
        
-        contenedorDeSkins.sprite = sprites[indiceSkin];
+        //contenedorDeSkins.sprite = sprites[indiceSkin];
         
     }
 
@@ -79,16 +84,41 @@ public class SkinRequest : MonoBehaviour
                   
                     print(player.nickName);
                     playerName.text = player.nickName;
-                    skinName.text = player.playerSkins[indiceSkin].skin.name;
-                    skinCode.text = player.playerSkins[indiceSkin].skin.code;
-                    print(player.playerSkins[indiceSkin].skin.name);
-                    print(indiceSkin);
+                    Skin[] skins = new Skin[player.playerSkins.Length];
+                    for(int i=0; i<skins.Length; i++)
+                    {
+                        skins[i] = player.playerSkins[i].skin;
+                    }
+                  
+                    //print(player.playerSkins[indiceSkin].skin.name);
+                    //print(indiceSkin);
 
-
+                    ViewSkinDate(skins);
 
 
                     break;
             }
         }
+    }
+
+    public void ViewSkinDate(Skin[] skins)
+    {
+        skinView = new GameObject[skins.Length];
+        contenedorDeSkins = new Image[skins.Length];
+        
+        for (int i = 0; i < skins.Length; i++)
+        {
+            skinView[i] = skinConteiner;
+            skinView[i].GetComponent<GetDatesSkin>().nameS.text = skins[i].name;
+            skinView[i].GetComponent<GetDatesSkin>().code.text = skins[i].code;
+            skinView[i].GetComponent<GetDatesSkin>().spriteSkin.sprite = sprites[skins[i].id];
+            Instantiate(skinView[i], new Vector3(pivotTextSkin.transform.position.x, pivotTextSkin.transform.position.y - 30 * (i + 1), pivotTextSkin.transform.position.z), transform.rotation, padreTextCanvas.transform);
+
+            Debug.Log("Nombre: "+skins[i].name + "Code: " + skins[i].code);
+            
+        }
+
+        //    skinName.text = skin.name;
+        //skinCode.text = skin.code;
     }
 }
--- P7WebServiceUnity/Assets/Scripts/Skins.cs
+++ P7WebServiceUnity/Assets/Scripts/Skins.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class skins 
+[CreateAssetMenu(fileName = "New DatesSkin", menuName = "Dskin")]
+public class skins: ScriptableObject
 {
-    public int Id;
+ 
     public string Name;
     public string Code;
 }
